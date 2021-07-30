3e42f065246ec25f766257f9a3d82d07aac6cdbe
Indra Sudirman <indrabsudirman@gmail.com>
2021-07-30 15:55:16 +0700

    RecyclerView in main activity

--- app/src/main/java/com/example/recyclerviewadditemfromotheractivity/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewadditemfromotheractivity/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    private ArrayList<ModelItemMurojaah> modelItemMurojaahArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,30 +31,44 @@ public class MainActivity extends AppCompatActivity {
         View view = activityMainBinding.getRoot();
         setContentView(view);
 
-        ArrayList<ModelItemMurojaah> modelItemMurojaahArrayList = new ArrayList<>();
+        createMurojaahArrayList();
+        buildRecyclerView();
+
+
+
+
+
+        activityMainBinding.buttonNext.setOnClickListener(view1 -> {
+            Intent intent = new Intent(getApplicationContext(), AddRecyclerViewItem.class);
+            startActivity(intent);
+
+        });
+    }
+    public void createMurojaahArrayList() {
+        modelItemMurojaahArrayList = new ArrayList<>();
         modelItemMurojaahArrayList.add(new ModelItemMurojaah("1", "Al-Fatihah", "1 - 7"));
         modelItemMurojaahArrayList.add(new ModelItemMurojaah("2", "Al-Baqarah", "1 - 10"));
         modelItemMurojaahArrayList.add(new ModelItemMurojaah("3", "Al-Kafirun", "1 - 5"));
         modelItemMurojaahArrayList.add(new ModelItemMurojaah("4", "An-Naas", "1 - 4"));
         modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+        modelItemMurojaahArrayList.add(new ModelItemMurojaah("5", "Al-Fiil", "1 - 6"));
+    }
 
+    public void buildRecyclerView() {
         recyclerView = activityMainBinding.recyclerViewListMurojaah;
         layoutManager = new LinearLayoutManager(this);
         adapter = new RecyclcerViewAdapter(modelItemMurojaahArrayList);
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-
-        activityMainBinding.buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AddRecyclerViewItem.class);
-                startActivity(intent);
-
-            }
-        });
     }
-//    void indra() {
-//        activityMainBinding
-//    }
+
 }
